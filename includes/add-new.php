9fb85203528a55be78b603435f7bbf698d449fd5
rootorchild <thanh.lvtr@gmail.com>
2019-01-03 11:45:11 +0700

    Create add-new.php

---
+++ includes/add-new.php
@@ -0,0 +1,32 @@
+<?php
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+require('../../../../wp-load.php');
+header("Location: /wp-admin/admin.php?page=extra-post-info");
+
+global $wpdb;
+
+$input1 = $_POST['input1'];
+$input2 = $_POST['input2'];
+$function_file = get_stylesheet_directory() . '/functions.php';
+// Write information to database
+$content_input = stripslashes($input2);
+$title_input = stripslashes($input1);
+$table_name = $wpdb->prefix . 'mbr_function_control';
+
+$wpdb->insert(
+    $table_name,
+    array(
+        'status'  => 1,
+        'open'    => 1,
+        'same'    => 1,
+        'title'   => $title_input,
+        'content' => $content_input,
+    )
+);
+
+// Write information to file funtions.php
+$function_content = PHP_EOL . '// ' . $input1 . PHP_EOL . $input2 . PHP_EOL;
+$function_content = stripslashes(preg_replace('/(\r\n|\r|\n)/s',"\n",$function_content));
+file_put_contents($function_file, $function_content, FILE_APPEND | LOCK_EX);
+?>
